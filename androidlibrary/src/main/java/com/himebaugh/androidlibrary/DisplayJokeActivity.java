767c9b85bd5748a3a27fdb71ff93d81100f80bec
Langdon Himebaugh <lang@himebaugh.com>
2018-08-07 19:29:59 -0400

    Step 3: Setup GCE
    >> https://classroom.udacity.com/nanodegrees/nd801/parts/8995005d-dc19-4c5f-b912-183c9a421c04/modules/429520885375461/lessons/4295208853239847/concepts/42732622150923
    
    2.1. Testing your app in an emulator COMPLETE
    
    >> https://github.com/GoogleCloudPlatform/gradle-appengine-templates/tree/77e9910911d5412e5efede5fa681ec105a0f02ad/HelloEndpoints#2-connecting-your-android-app-to-the-backend

--- androidlibrary/src/main/java/com/himebaugh/androidlibrary/DisplayJokeActivity.java
+++ androidlibrary/src/main/java/com/himebaugh/androidlibrary/DisplayJokeActivity.java
@@ -1,12 +1,20 @@
 package com.himebaugh.androidlibrary;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.NavUtils;
 import android.widget.TextView;
 
 public class DisplayJokeActivity extends AppCompatActivity {
+
+    private final static String TAG = DisplayJokeActivity.class.getName();
 
     public static final String EXTRA_JOKE_STRING = "extra_joke_string";
 
@@ -21,6 +29,30 @@ public class DisplayJokeActivity extends AppCompatActivity {
         if (intent != null && intent.hasExtra(EXTRA_JOKE_STRING)) {
             jokeTextView.setText(intent.getStringExtra(EXTRA_JOKE_STRING));
         }
+
+        // Show the Up button in the action bar.
+//        ActionBar actionBar = getSupportActionBar();
+//        if (actionBar != null) {
+//            actionBar.setDisplayHomeAsUpEnabled(true);
+//        }
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        //getCallingActivity().
+//
+//        this.onBackPressed();
+        
+
+//        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //getActivity().onBackPressed();
+//                //this.onBackPressed();
+//
+//                Log.i(TAG, "onClick: ");
+//            }
+//        });
 
     }
 
